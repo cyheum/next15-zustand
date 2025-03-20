f5f59bc10f275a896d5988d46594eee281ff0899
cyheum <coffee8357@gmail.com>
2025-03-20 17:07:35 +0900

    [ENH] : react hook form + 기본  validation 추가

--- src/components/layout/Home/index.tsx
+++ src/components/layout/Home/index.tsx
@@ -1,16 +1,32 @@
 'use client';
 
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-import { Button } from '@/components';
-import { Slider } from '@/components';
+import { Button, Slider } from '@/components';
 import { useHomeStore } from '@/store';
+import { rexName } from '@/utils';
+
+interface IFormInput {
+  firstName: string;
+  gender: string;
+}
 
 export const HomeContainer: React.FC = () => {
+  const { register, handleSubmit, formState } = useForm<IFormInput>({
+    mode: 'all',
+  });
+  const { errors } = formState;
   const { count, actions } = useHomeStore();
   const styles = {
     container: 'm-5',
   };
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
+
+  console.log(formState);
 
   return (
     <>
@@ -34,6 +50,40 @@ export const HomeContainer: React.FC = () => {
             clickable: true,
           }}
         />
+        <div className="pt-10">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label className="block mb-[10]">First Name</label>
+            <input
+              {...register('firstName', {
+                required: '닉네임을 입력해주세요',
+                minLength: { value: 2, message: '2글자 이상 입력해주세요' },
+                maxLength: { value: 8, message: '최대 8글자입니다.' },
+                pattern: {
+                  // input의 정규식 패턴
+                  value: rexName,
+                  message: '가능한 문자: 영문 대소문자, 숫자', // 에러 메세지
+                },
+              })}
+              className="block border border-solid rounded-sm mb-[10]"
+              aria-invalid={errors.firstName ? 'true' : 'false'}
+            />
+            <p className="text-red-600" role="alert">
+              {errors.firstName?.message ?? ''}
+            </p>
+            <label className="block mb-[10]">Gender Selection</label>
+            <select
+              {...register('gender')}
+              className="block border border-solid rounded-md p-[4] mb-[10]"
+            >
+              <option value="female">female</option>
+              <option value="male">male</option>
+              <option value="other">other</option>
+            </select>
+            <button className="border border-solid rounded-[10] px-[1rem] py-[0.5rem]">
+              제출
+            </button>
+          </form>
+        </div>
       </main>
     </>
   );
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,11 @@
+import {
+  rexBirth,
+  rexEamil,
+  rexName,
+  rexPhoneNumber,
+  rexPostcode,
+} from './validation';
+
 export function toBodyStyleHidden(onOff: boolean) {
   const body = document.querySelector('body') as HTMLElement;
 
@@ -7,5 +15,38 @@ export function toBodyStyleHidden(onOff: boolean) {
     body.style.height = '100%';
   } else {
     body.removeAttribute('style');
+  }
+}
+
+export function validateEmail(email: string) {
+  const re = rexEamil;
+  return re.test(email);
+}
+
+export function validatePhoneNumber(phoneNumber: string) {
+  const re = rexPhoneNumber;
+  return re.test(phoneNumber);
+}
+
+export function validatePostcode(validatePostcode: string) {
+  const re = rexPostcode;
+  return re.test(validatePostcode);
+}
+
+export function checkBirth(str: string) {
+  const regExp = rexBirth;
+  if (regExp.test(str)) {
+    return true;
+  } else {
+    false;
+  }
+}
+
+export function checkName(str: string) {
+  const regExp = rexName;
+  if (regExp.test(str)) {
+    return true;
+  } else {
+    false;
   }
 }
